31988dc42f2c5b958cc4e2745d362b77afb4b296
robbert <robbert@vdzon.com>
2015-04-02 20:26:38 +0200

    update

--- docker/cluster_build.sh
+++ docker/cluster_build.sh
@@ -45,7 +45,7 @@ docker run -d -p $MYSQL_SSH_PORT1:22 -p $MYPHPADMIN_PORT1:80 -p 172.17.42.1:1330
 
 # build and run mysql container
 docker build -t robbertvdzon/$DOCKERNAME_MYSQLDB2 ./mysql
-docker run -d -p $MYSQL_SSH_PORT2:22 -p $MYPHPADMIN_PORT2:80 -p 172.17.42.1:13306:4306 --name $DOCKERNAME_MYSQLDB2 robbertvdzon/$DOCKERNAME_MYSQLDB2
+docker run -d -p $MYSQL_SSH_PORT2:22 -p $MYPHPADMIN_PORT2:80 -p 172.17.42.1:23306:3306 --name $DOCKERNAME_MYSQLDB2 robbertvdzon/$DOCKERNAME_MYSQLDB2
 
 # build and run wildfly container
 docker build -t robbertvdzon/$DOCKERNAME_WILDFLY1 ./wildfly
