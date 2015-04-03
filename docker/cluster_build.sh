0e87c29633199c6cf9f4e3152d814c0286b605b0
robbert <robbert@vdzon.com>
2015-04-03 13:54:20 +0200

    update

--- docker/cluster_build.sh
+++ docker/cluster_build.sh
@@ -61,7 +61,7 @@ docker build -t robbertvdzon/$DOCKERNAME_MYSQL_HAPROXY ./mysqlhaproxy
 echo ---------2
 echo docker run -d -it -p $HAPROXY_MYSQL_PORT:3306 -p $HAPROXY_MYSQL_ADMIN_PORT:82 --name $DOCKERNAME_MYSQL_HAPROXY --link $DOCKERNAME_MYSQLDB1:mysqldb1 --link $DOCKERNAME_MYSQLDB2:mysqldb1 robbertvdzon/$DOCKERNAME_MYSQL_HAPROXY
 
-docker run -d -it -p $HAPROXY_MYSQL_PORT:3306 -p $HAPROXY_MYSQL_ADMIN_PORT:82 --name $DOCKERNAME_MYSQL_HAPROXY --link $DOCKERNAME_MYSQLDB1:mysqldb1 --link $DOCKERNAME_MYSQLDB2:mysqldb1 robbertvdzon/$DOCKERNAME_MYSQL_HAPROXY
+docker run -d -it -p $HAPROXY_MYSQL_PORT:3306 -p $HAPROXY_MYSQL_ADMIN_PORT:82 --name $DOCKERNAME_MYSQL_HAPROXY --link $DOCKERNAME_MYSQLDB1:mysqldb1 --link $DOCKERNAME_MYSQLDB2:mysqldb2 robbertvdzon/$DOCKERNAME_MYSQL_HAPROXY
 
 
 # build and run wildfly container
