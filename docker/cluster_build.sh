5e4f8ad464742ad6e14925dcff21730875954a27
robbert <robbert@vdzon.com>
2015-04-03 18:37:32 +0200

    update

--- docker/cluster_build.sh
+++ docker/cluster_build.sh
@@ -49,14 +49,12 @@ docker build -t robbertvdzon/$DOCKERNAME_MYSQLDB1 ./mysql1
 docker run -d -p $MYSQL_SSH_PORT1:22 -p $MYPHPADMIN_PORT1:80 -p 172.17.42.1:13306:3306 --name $DOCKERNAME_MYSQLDB1 robbertvdzon/$DOCKERNAME_MYSQLDB1
 
 # build and run mysql container
-docker build -t robbertvdzon/$DOCKERNAME_MYSQLDB2 ./mysql2
-docker run -d -p $MYSQL_SSH_PORT2:22 -p $MYPHPADMIN_PORT2:80 -p 172.17.42.1:23306:3306 --name $DOCKERNAME_MYSQLDB2 robbertvdzon/$DOCKERNAME_MYSQLDB2
+#docker build -t robbertvdzon/$DOCKERNAME_MYSQLDB2 ./mysql2
+#docker run -d -p $MYSQL_SSH_PORT2:22 -p $MYPHPADMIN_PORT2:80 -p 172.17.42.1:23306:3306 --name $DOCKERNAME_MYSQLDB2 robbertvdzon/$DOCKERNAME_MYSQLDB2
 
 # build and run mysql haproxy container
-
-
-docker build -t robbertvdzon/$DOCKERNAME_MYSQL_HAPROXY ./mysqlhaproxy
-docker run -d -it -p $HAPROXY_MYSQL_PORT:3306 -p $HAPROXY_MYSQL_ADMIN_PORT:82 --name $DOCKERNAME_MYSQL_HAPROXY robbertvdzon/$DOCKERNAME_MYSQL_HAPROXY
+#docker build -t robbertvdzon/$DOCKERNAME_MYSQL_HAPROXY ./mysqlhaproxy
+#docker run -d -it -p $HAPROXY_MYSQL_PORT:3306 -p $HAPROXY_MYSQL_ADMIN_PORT:82 --name $DOCKERNAME_MYSQL_HAPROXY robbertvdzon/$DOCKERNAME_MYSQL_HAPROXY
 
 
 # build and run wildfly container
@@ -77,5 +75,6 @@ docker run -d -it -p $APACHE_SSH_PORT1:22 -p $APACHE_PORT1:80 --link $DOCKERNAME
 #docker run -d -it -p $APACHE_SSH_PORT2:22 -p $APACHE_PORT2:80 --link $DOCKERNAME_WILDFLY2:wildfly --name $DOCKERNAME_APACHE2 robbertvdzon/$DOCKERNAME_APACHE2
 
 # build and run haproxy container
-#docker build -t robbertvdzon/$DOCKERNAME_HAPROXY ./haproxy
+docker build -t robbertvdzon/$DOCKERNAME_HAPROXY ./haproxy
 #docker run -d -it -p $HAPROXY_PORT:80 -p $HAPROXY_ADMIN_PORT:81 --name $DOCKERNAME_HAPROXY --link $DOCKERNAME_APACHE1:apache1 --link $DOCKERNAME_APACHE2:apache2 robbertvdzon/$DOCKERNAME_HAPROXY
+docker run -d -it -p $HAPROXY_PORT:80 -p $HAPROXY_ADMIN_PORT:81 --name $DOCKERNAME_HAPROXY --link $DOCKERNAME_APACHE1:apache1 --link $DOCKERNAME_WILDFLY1:wildfly1 robbertvdzon/$DOCKERNAME_HAPROXY
